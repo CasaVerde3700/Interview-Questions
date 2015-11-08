a1350d67a723f30dd846be12288ac7c4ab35b3ee
Anqi Yan <yaq0925@gmail.com>
2015-11-08 14:39:25 -0800

    Trie data structure

---
+++ Trie.java
@@ -0,0 +1,50 @@
+class TrieNode {
+    public TrieNode[] children = new TrieNode[26];
+    String s = "";
+
+    public TrieNode() {}
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        
+        node.s = word;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        
+        return node.s.equals(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        
+        return true;
+    }
+}
